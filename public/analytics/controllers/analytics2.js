49ecc5e0ae96c717484de06c8166446eab40e178
Xiuli Shen <xiulishen@Xiulis-MacBook-Pro.local>
2014-05-03 20:31:10 -0700

    Updated graph data

---
+++ public/analytics/controllers/analytics2.js
@@ -0,0 +1,25 @@
+/*'use strict';
+
+angular.module('mean.analytics').controller('AnalyticsController', ['$scope', '$stateParams', '$location', 'Global', 'd3',
+    function($scope, $stateParams, $location, Global) {
+        $scope.global = Global;
+        // Default hard coded menu items for main menu
+        // hardcoded student data to test
+        $scope.students = [{
+            'id': 1,
+            'score': 0.5,
+            'name': 'Jenny'
+        }, {
+            'id': 2,
+            'score': 0.8,
+            'name': 'Matt'
+        }];
+        $scope.students2 = [{
+            id: 1,
+            name: 'Jenny'
+        }, {
+            id: 2,
+            name: 'Matt'
+        }];
+    }
+]);*/
